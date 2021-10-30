cf569792fa3641539b55cd2b4d16e32f01e611f3
Anna Dzierżawa <annadzierzawa@gmail.com>
2021-10-30 17:33:54 +0200

    pet form

--- Petify.Web/src/app/material.module.ts
+++ Petify.Web/src/app/material.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from "@angular/core";
-import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from "@angular/material-moment-adapter";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
-import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatFormFieldDefaultOptions, MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MAT_NATIVE_DATE_FORMATS } from "@angular/material/core";
 
+const appearance: MatFormFieldDefaultOptions = {
+    appearance: "outline"
+};
+
+const DATE_PICKER_FORMAT = {
+    parse: {
+        dateInput: ["DD.MM.YYYY"]
+    },
+    display: {
+        ...MAT_NATIVE_DATE_FORMATS.display,
+        dateInput: "DD.MM.YYYY",
+        monthYearLabel: "MMM YYYY",
+        dateA11yLabel: "DD.MM.YYYY",
+        monthYearA11yLabel: "MMMM YYYY",
+    },
+};
 
 @NgModule({
     imports: [
@@ -33,6 +50,20 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
         MatInputModule,
         MatMenuModule,
         MatDatepickerModule
-    ]
+    ],
+    providers: [
+        {
+            provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+            useValue: appearance
+        },
+        { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+        { provide: MAT_DATE_LOCALE, useValue: "pl-PL" },
+        {
+            provide: DateAdapter,
+            useClass: MomentDateAdapter,
+            deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+        },
+        { provide: MAT_DATE_FORMATS, useValue: DATE_PICKER_FORMAT }
+    ],
 })
 export class MaterialModule { }
